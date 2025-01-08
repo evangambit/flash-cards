801c67150951145ad07ffaf5e1aa756f69004f9b
Morgan Redding <evangambit@gmail.com>
2025-01-08 20:50:21 +0800

    Have service worker automatically update its cache

--- frontend/serviceworker.js
+++ frontend/serviceworker.js
@@ -1,23 +1,5 @@
-// https://googlechrome.github.io/samples/service-worker/basic/
 
-/*
- Copyright 2016 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-// Names of the two caches used in this version of the service worker.
-// Change to v2, etc. when you update any of the local resources, which will
-// in turn trigger the install event again.
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = 'flash-cache';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -27,51 +9,45 @@ const PRECACHE_URLS = [
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(PRECACHE)
+    caches.open(CACHE_NAME)
       .then(cache => {
-        console.log(cache);
-        console.log(PRECACHE_URLS);
         cache.addAll(PRECACHE_URLS);
       })
       .then(self.skipWaiting())
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin) && event.request.method === 'GET') {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+// If a cache response exists, returns it, otherwise fetches result from server.
+// If a cache response exists, try to fetch and update it (subsequent requests will
+// get the updated response).
+// Inpsired by https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate
+self.addEventListener('fetch', (event) => {
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
   }
+  // Skip non-GET requests.
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  event.respondWith(
+    (async function () {
+      const cache = await caches.open('mysite-dynamic');
+      const cachedResponse = await cache.match(event.request);
+      const networkResponsePromise = fetch(event.request).catch(err => {
+        console.warn(err);
+      });
+
+      event.waitUntil(
+        (async function () {
+          const networkResponse = await networkResponsePromise;
+          if (networkResponse) {
+            await cache.put(event.request, networkResponse.clone());
+          }
+        })(),
+      );
+
+      // Returned the cached response if we have one, otherwise return the network response.
+      return cachedResponse || networkResponsePromise;
+    })(),
+  );
 });
